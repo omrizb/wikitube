368b2f5af9f02fd54029999f572d44dd14abe8f1
omribarouch <omrizb@gmail.com>
2024-04-12 18:06:00 +0300

    CSS touches

--- js/services/wiki.service.js
+++ js/services/wiki.service.js
@@ -3,10 +3,6 @@
 function getWiki(searchVal, numOfResults) {
     return axios.get(`https://en.wikipedia.org/w/api.php?&origin=*&action=query&list=search&srsearch=${searchVal}&format=json`)
         .then(res => {
-            console.log(res.data)
-            return res
-        })
-        .then(res => {
             const entries = []
             for (let i = 0; i < numOfResults; i++) {
                 entries.push({
